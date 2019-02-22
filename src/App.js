740dedf99a847e5650755f54e6b93aaa6b5deadc
gonzalo damiano <gondamiano@gmail.com>
2019-02-22 14:01:38 +0100

    take react logo and index from create react app. fix the credt card logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Input from './helpers/input.js';
 import ExpirationInput from './helpers/expirationInput.js';
@@ -9,20 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Input name="gonza" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Input />
           <ExpirationInput />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
